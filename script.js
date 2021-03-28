c8bcf7e32831fcec53eb807bd429729c9e8f155b
Bladerendeeg <41634346+Bladerendeeg@users.noreply.github.com>
2021-03-28 12:18:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// const overHundred = function (argument) {
+//     if (argument > 100) {
+//         return true;
+//     }
+//     return false;
+// };
+// const result = overHundred(111);
+// console.log(result);
+
+const brendaBot = function (max, current, age) {
+    if (max < 100) {
+        console.log("Come in");
+        return max;
+    } else if (current >= 100) {
+        console.log("it's too busy now, come back later");
+        return current;
+    } else if (age >= 18) {
+        console.log("this is a club for adults");
+        return age;
+    } else {
+        console.log("Emptyness");
+    }
+}
+const result2 = brendaBot(979, 12, 17);
+
+const avg = function ([n1, n2, n3, n4, n5]) {
+    const numbers = [1, 2, 3, 4, 5];
+    avg.numbers /.length;
+
+}
+const result3 = (2, 2, 2, 2, 3)
+
